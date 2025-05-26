656c5b3698b5c10f3b7a1cbf5ac09bc6e7cba545
Siva D. Nithish <sivanithishkumar12@gmail.com>
2025-05-26 14:02:36 +0000

    all your pages are slightly moved towards left , it includes all pages , the feeds , profile , friends , agents etc..,
    so align all the pages properly

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -38,7 +38,7 @@ export default function RootLayout({
         <AuthProvider>
           <div className="relative flex min-h-screen flex-col">
             <Header />
-            <main className="flex-1 container py-8">{children}</main>
+            <main className="flex-1 container mx-auto py-8">{children}</main>
           </div>
           <Toaster />
         </AuthProvider>
